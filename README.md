58b389f13dff26c6d50deedcdd4dbafbf705defb
uetomae-naohiko-mori <naohiko.mori@uetomae.co.jp>
2018-12-27 17:01:13 +0900

    add additional modules to guide getting started easier

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # UETOMAE Automata
+
+Automata is designed to be simple to integrate automated end-to-end testing. The developers do not need to create and manage the environment. They can just focus on writing test cases.
+
+## Getting Started
+
+See the [wiki page](https://github.com/uetomae/automata/wiki) and follow the setup process.
+
---
+++ bin/bs_list_devices.py
@@ -0,0 +1,18 @@
+import os
+import sys
+import json
+import urllib2, base64
+import csv
+
+bs_username = os.environ['BROWSER_STACK_USERNAME']
+bs_access_key = os.environ['BROWSER_STACK_ACCESS_KEY']
+
+req = urllib2.Request('https://api-cloud.browserstack.com/app-automate/devices.json')
+encoded_con = base64.encodestring('%s:%s' % (bs_username, bs_access_key)).replace('\n', '')
+req.add_header("Authorization", "Basic %s" % encoded_con)
+res = urllib2.urlopen(req)
+
+browsers = json.loads(res.read())
+
+for browser in browsers:
+    print '"%s", "%s", "%s"' % (browser['os'], browser['os_version'], browser['device'])
